d24778c2f967c84579d8dc40c7b203f9f2ea9c19
Ibnul Kayes <ikayes114@gmail.com>
2022-04-12 15:35:06 +0600

    Add example of HTTP status handling

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -12,8 +20,15 @@ export class CoffeesController {
     return `This action return ${id} coffee`;
   }
 
-  @Post()
+  //   @Post()
   create(@Body() body) {
     return body;
   }
+
+  // Example of handling HTTP status code
+  //   @Post()
+  //   @HttpCode(HttpStatus.GONE)
+  //   create(@Body() body) {
+  //     return body;
+  //   }
 }
